8967483f4179dfe7206a56866d1bbc51f3e6d4f3
Vivien <vivien@MacBook-Pro-de-Vivien.local>
2023-01-30 17:49:10 +0100

    Update credentials for mongodb

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
-dbPassword = "mongodb+srv://avk:" + encodeURIComponent('JTd86VrPiMxsBhHx') + "@cluster0.ri4bg7m.mongodb.net/qrtour?retryWrites=true&w=majority";
+dbPassword = "mongodb+srv://qrtour:" + encodeURIComponent('vQQ6RLc2w5Tz5Ouk') + "@cluster0.ksko1ps.mongodb.net/?retryWrites=true&w=majority";
 
 
 module.exports = {
